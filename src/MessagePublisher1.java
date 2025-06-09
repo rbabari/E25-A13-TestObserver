b863aed5faf473afe1bdbbffc6922a04ed252d4a
rbabari <r.babari@gmail.com>
2025-06-09 16:37:18 -0400

    mermaid uml

--- src/MessagePublisher1.java
+++ src/MessagePublisher1.java
@@ -2,6 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MessagePublisher1 implements Observable {
+
+   /*
+   Le corps de ton publisher :: exemple un Bouton d'un menu ....
+
+
+
+    */
+
+
+
+
     private List<Observer> observers = new ArrayList<Observer>();
 
     @Override
